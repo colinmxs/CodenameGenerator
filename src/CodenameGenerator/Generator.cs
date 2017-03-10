7616311121d8239e9e80cc40978aca3ebc49d8cb
Colin Smith <smith.colin00@gmail.com>
2017-03-09 22:02:58 -0700

    created first tests

---
+++ src/CodenameGenerator/Generator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CodenameGenerator
+{
+    public class Generator
+    {
+        public string Generate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] GenerateMany(int count)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tests/CodenameGenerator.Tests/GeneratorTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CodenameGenerator.Tests
+{
+    [TestClass]
+    public class GeneratorTests
+    {
+        private Generator _generator;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _generator = new Generator();
+        }
+
+        [TestMethod]
+        public void Generate()
+        {
+            string result = _generator.Generate();
+
+            Assert.IsNotNull(result);
+            Assert.AreNotEqual("", result);
+        }
+
+        [TestMethod]
+        public void GenerateMany(int count)
+        {
+
+            string[] results = _generator.GenerateMany(count);
+            Assert.IsNotNull(results);
+            Assert.AreEqual(count, results.Length);
+            foreach (var result in results)
+            {
+                Assert.AreNotEqual("", result);
+            }
+        }    
+    }
+}
