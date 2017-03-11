0e7d9357b108546c121ded1e19e46f74a3600290
Colin Smith <smith.colin00@gmail.com>
2017-03-10 20:58:39 -0700

    moar tests!

--- src/CodenameGenerator/Generator.cs
+++ src/CodenameGenerator/Generator.cs
@@ -4,12 +4,25 @@ namespace CodenameGenerator
 {
     public class Generator
     {
+        public char Separator { get; set; }
+        public int WordCount { get; set; }
+
         public string Generate()
         {
             throw new NotImplementedException();
         }
 
         public string[] GenerateMany(int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetWordCount(int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetSeparator(char separator)
         {
             throw new NotImplementedException();
         }
--- tests/CodenameGenerator.Tests/GeneratorTests.cs
+++ tests/CodenameGenerator.Tests/GeneratorTests.cs
@@ -18,7 +18,6 @@ namespace CodenameGenerator.Tests
         public void Generate()
         {
             string result = _generator.Generate();
-
             Assert.IsNotNull(result);
             Assert.AreNotEqual("", result);
         }
@@ -34,6 +33,56 @@ namespace CodenameGenerator.Tests
             {
                 Assert.AreNotEqual("", result);
             }
-        }    
+        }
+
+        [TestMethod]
+        public void Generate_SpecifyWordCount(int count)
+        {
+            _generator.SetWordCount(count);
+            string result = _generator.Generate();
+            char separator = _generator.Separator;
+            var strings = result.Split(separator);
+            Assert.AreEqual(count, strings.Length);
+        }
+
+        [TestMethod]
+        public void GenerateMany_SpecifyWordCount(int count1, int count2)
+        {
+            _generator.SetWordCount(count2);
+            Assert.AreEqual(count2, _generator.WordCount);
+            string[] results = _generator.GenerateMany(count1);
+            char separator = _generator.Separator;
+            foreach (var result in results)
+            {
+                var strings = result.Split(separator);
+                Assert.AreEqual(count2, strings.Length);
+            }            
+        }
+
+        [TestMethod]
+        public void Generate_SpecifySeparator(char separator)
+        {
+            _generator.SetSeparator(separator);
+            string result = _generator.Generate();
+            Assert.IsTrue(result.Contains(separator.ToString()));
+        }
+
+        [TestMethod]
+        public void GenerateMany_SpecifySeparator(int count, char separator)
+        {
+            _generator.SetSeparator(separator);
+            var results = _generator.GenerateMany(count);
+            foreach (var result in results)
+            {
+                Assert.IsTrue(result.Contains(separator.ToString()));
+            }
+        }
+
+        [TestMethod]
+        public void Generate_Defaults(char separator)
+        {
+            _generator.Separator = ' ';
+            _generator.WordCount = 2;
+        }
     }
 }
