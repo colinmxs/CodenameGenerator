9a131b68266965cfc17b1077f611883b2baf316b
colinsmith1 <colinsmith1@boisestate.edu>
2017-03-13 11:22:44 -0600

    removed unused using

--- src/CodenameGenerator/WordBank.cs
+++ src/CodenameGenerator/WordBank.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace CodenameGenerator
+﻿namespace CodenameGenerator
 {    
     //http://stackoverflow.com/questions/38855909/alternative-to-enum-in-design-pattern
     public class WordBank
@@ -9,11 +7,11 @@ namespace CodenameGenerator
         protected readonly Word Value;
         protected readonly IWordRepository Repo;
 
-        public static readonly WordBank Nouns = new WordBank(Word.Nouns, "Nouns", new FileRepository("nouns.csv"));
-        public static readonly WordBank Adjectives = new WordBank(Word.Adjectives, "Adjectives", new FileRepository("adjectives.csv"));
-        public static readonly WordBank FirstNames = new WordBank(Word.FirstNames, "FirstNames", new FileRepository("firstnames.csv"));
-        public static readonly WordBank LastNames = new WordBank(Word.LastNames, "LastNames", new FileRepository("lastnames.csv"));
-        public static readonly WordBank Titles = new WordBank(Word.Titles, "Titles", new TitleRepository());
+        public static readonly WordBank Nouns = new WordBank(Word.Noun, "Nouns", new FileRepository("nouns.csv"));
+        public static readonly WordBank Adjectives = new WordBank(Word.Adjective, "Adjectives", new FileRepository("adjectives.csv"));
+        public static readonly WordBank FirstNames = new WordBank(Word.FirstName, "FirstNames", new FileRepository("firstnames.csv"));
+        public static readonly WordBank LastNames = new WordBank(Word.LastName, "LastNames", new FileRepository("lastnames.csv"));
+        public static readonly WordBank Titles = new WordBank(Word.Title, "Titles", new TitleRepository());
 
         protected WordBank(Word value, string name, IWordRepository repo)
         {
